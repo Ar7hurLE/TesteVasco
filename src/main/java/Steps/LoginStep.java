e54f97d51bc67b46ebad8bbe130412e8ca6eb94a
Arthur Elias <arthurelias777@gmail.com>
2021-12-28 16:16:50 -0300

    runners,steps e features funcionando

--- src/main/java/Steps/LoginStep.java
+++ src/main/java/Steps/LoginStep.java
@@ -14,37 +14,37 @@ public class LoginStep {
 
     WebDriver driver;
 
-    @Given("estamos no google")
+    @Given("^estamos no google$")
     public void estamos_no_google() throws Throwable {
-        System.setProperty("webdriver.chhrome.driver", "C:/Users/HP/IdeaProjects/TesteVasco/src/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:/Users/HP/IdeaProjects/TesteVasco/src/drivers/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://google.com/");
     }
 
-        @When("procuramos por 'vasco'")
-                public void pesquisa() throws Throwable {
-            WebElement Busca = driver.findElement(By.id("input"));
-            Busca.sendKeys("Vasco");
-            Busca.submit();
-        }
-
-        @And("clicamos no resultado que obtém url 'https://vasco.com.br/'$")
-    public void entrar_site() throws Throwable{
-        driver.findElement(By.xpath("//*[@id=\"rso\"]/div[4]/div/div/div[1]/a/h3")).click();
+    @When("^procuramos por vasco da gama$")
+    public void procuramos_por_vasco_da_gama() throws Throwable {
+        WebElement Busca = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+        Busca.sendKeys("Vasco da gama");
+        Busca.submit();
     }
 
-        @And("entramos no site")
-    public void verifica() throws Throwable{
-            Assert.assertFalse(driver.getCurrentUrl().equals("https://google.com/"));
-            Assert.assertTrue(driver.getCurrentUrl().equals("https://vasco.com/"));
-        }
+    @And("^clicamos no resultado que obtém url do site oficial$")
+    public void clicamos_no_resultado_que_obtém_url_do_site_oficial() throws Throwable {
+        driver.findElement(By.xpath("//*[@id=\"rso\"]/div[3]/div/div[1]/div/div/div/div[1]")).click();
+    }
 
-        @Then("o brasão do Vasco deve estar lá")
-        public void brasao() throws Throwable{
-        Assert.assertEquals(By.id("navigation-sticky"), driver.findElement(By.id("navigation-sticky")));
+    @And("^entramos no site$")
+    public void entramos_no_site() throws Throwable {
+        Assert.assertFalse(driver.getCurrentUrl().equals("https://google.com/"));
+    }
+
+    @Then("^o brasão do Vasco deve estar lá$")
+    public void o_brasão_do_Vasco_deve_estar_lá() throws Throwable {
+        driver.findElement(By.xpath("//*[@id=\"navigation-sticky\"]/div/div/div/div[2]/div/a/img"));
         driver.quit();
-        }
-
     }
+}
+
+
 
 
--- src/main/java/features/Apontamentos.feature
+++ src/main/java/features/Apontamentos.feature
@@ -3,7 +3,7 @@ Feature: FEATURE DE VERIFICAÇÃO
   Scenario: Verificar se o brasão do vasco encontra-se no site
 
     Given estamos no google
-    When procuramos por "vasco"
-    And clicamos no resultado que obtém url "https://vasco.com.br/"
+    When procuramos por vasco da gama
+    And clicamos no resultado que obtém url do site oficial
     And entramos no site
     Then o brasão do Vasco deve estar lá
